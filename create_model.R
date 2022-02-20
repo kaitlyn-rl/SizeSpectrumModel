407b790b3709214f563a07653a8a348a660d4a87
krl516 <louthkaitlyn@gmail.com>
2022-02-20 16:00:18 +0000

    Adding interaction matrix as a model parameter

--- create_model.R
+++ create_model.R
@@ -3,19 +3,21 @@ install_github("sizespectrum/mizerExperimental")
 install_github("sizespectrum/mizer")
 library(dplyr)
 library(mizer)
-library(mizerExperimental)
+library(mizerExperimental) #has some additional features to mizer
 
 library(readxl)
 Model_params <- read_excel("Model params.xlsx", range = "A1:I11")
 
-sp <- Model_params
+sp <- Model_params #creates a data frame of the original model params
+
+#Need to rename the columns in the data frame to fit the arguments of mizer (see cheat sheet names)
 sp$species <- sp$'Species of interest'
 sp$w_inf <- sp$W_infinity
 
 # Improve these a and b values later
+# mizer assumes a = 0.001 and b=3
 sp$a <- 0.006
 sp$b <- 3
-
 sp$w_mat <- sp$a * sp$L_mat ^ sp$b
 sp$w_mat
 
@@ -28,12 +30,14 @@ gear_params(params)
 gear_params(params)$catchability <- sp$`Fishing (external) mortality rate`
 gear_params(params)$knife_edge_size <- as.numeric(sp$`Minimum landing size cm`)
 gear_params(params)$knife_edge_size[7] <- 25 #change later
+
+plotSpectra(params)
+
+params <- steady(params) #takes x years to find a steady state 
+plotSpectra(params)
 initial_effort(params) <- 1 #fishing effort 
 
-plotSpectra(params)
-
-params <- steady(params)
-plotSpectra(params)
+species_params(params)
 
 params <- tuneParams(params, controls = c("abundance", "predation", "reproduction", "other", "interaction", "resource"))
 #calibrate and match 
